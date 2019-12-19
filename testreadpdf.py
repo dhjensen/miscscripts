c0ee188e8cf72401fe20ba2837a7f95d4c9adf38
Daniel Jensen <dhjen@outlook.com>
2019-12-19 14:35:04 +0100

    Fixed: # TODO: Try out this link https://stackoverflow.com/questions/58226546/python-data-extraction-from-an-encrypted-pdf         Replace current patched version of pdfminer with example in URL.         It will help make script less complicated to maintain.

--- testreadpdf.py
+++ testreadpdf.py
@@ -5,15 +5,45 @@ https://github.com/pdfminer/pdfminer.six
 Usage: 
 
 Example: 
+
 """
 
-# TODO: Try out this link https://stackoverflow.com/questions/58226546/python-data-extraction-from-an-encrypted-pdf
+# # Have to comment out line 131-132 in pdfpage.py in pdfminer to extract text:
+#         # if check_extractable and not doc.is_extractable:
+#         #     raise PDFTextExtractionNotAllowed('Text extraction is not allowed: %r' % fp)
+# from pdfminer.high_level import extract_text
 
-# Have to comment out line 131-132 in pdfpage.py in pdfminer to extract text:
-        # if check_extractable and not doc.is_extractable:
-        #     raise PDFTextExtractionNotAllowed('Text extraction is not allowed: %r' % fp)
-from pdfminer.high_level import extract_text
-
-pdf_text = extract_text(pdf_file = 'pdfin\\15935_22801.pdf')
+# pdf_text = extract_text(pdf_file = 'pdfin\\15935_22801.pdf')
 
 # TODO: extract date string and find month + year
+
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import StringIO
+
+def extract_protected_pdf_text(path):
+    
+    output = StringIO()
+
+    resource_manager = PDFResourceManager()
+    laparams = LAParams()
+
+    device = TextConverter(resource_manager, output, codec='utf-8', laparams=laparams)
+
+    pdf_infile = open(path, 'rb')
+    interpreter = PDFPageInterpreter(resource_manager, device)
+
+    page_numbers = set()
+    
+    for page in PDFPage.get_pages(pdf_infile, page_numbers, maxpages=0, caching=True, check_extractable=False):
+        interpreter.process_page(page)
+
+    text = output.getvalue()
+    pdf_infile.close()
+    device.close()
+    output.close()
+    return text
+
+pdf_text = extract_protected_pdf_text(path = 'pdfin\\15935_22801.pdf')
