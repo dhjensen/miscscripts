c9539b2e57be9997ee47b8c45732a52f0e8f8e37
Daniel Jensen <dhjen@outlook.com>
2019-12-19 14:46:30 +0100

    Added two TODOS to lastsickday.py # TODO: Move this to other branch as it's not related to "gmailscript" # TODO: Add function to tell how long the doctors sick leave is for.

--- lastsickday.py
+++ lastsickday.py
@@ -1,6 +1,9 @@
 from datetime import date
 from datetime import timedelta
 
+# TODO: Move this to other branch as it's not related to "gmailscript"
+# TODO: Add function to tell how long the doctors sick leave is for.
+
 firstsickday = date(year = 2019, month = 10, day = 7)
 
 maxsickdays = timedelta(days = 120)
