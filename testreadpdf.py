fbd8a3b81d1dee51123a9598000ec4da4ecaf6d9
Daniel Jensen <dhjen@outlook.com>
2020-02-08 17:07:59 +0100

    Function extract_protected_pdf_text: Made it possibly to specify None as password.
    Function create_file_name
    Changed parameter name from text to text_or_filename
    Added generating filename from existing filename for other type of PDFs
    
    function main
    Added logic to handle files with different names.
    
    Did some genereal cleanup
    
    Fixed todos:
    TODO: Test extracting dates for filenames from a broather list of PDF documents
    Alternative description: pdfminer: Use https://github.com/pdfminer/pdfminer.six to get month / year from salary documents for naming purposes
    
    TODO: Split create_file_name() into two functions
    Ignored... problems fixed in the function

--- testreadpdf.py
+++ testreadpdf.py
@@ -13,13 +13,6 @@ Example:
 #         #     raise PDFTextExtractionNotAllowed('Text extraction is not allowed: %r' % fp)
 # from pdfminer.high_level import extract_text
 
-# pdf_text = extract_text(pdf_file = 'pdfin\\15935_22801.pdf')
-
-# TODO: Test extracting dates for filenames from a broather list of PDF documents
-# Alternative description: pdfminer: Use https://github.com/pdfminer/pdfminer.six to get month / year from salary documents for naming purposes
-
-# TODO: Split create_file_name() into two functions
-
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
@@ -47,9 +40,13 @@ def extract_protected_pdf_text(path: str, password: str) -> str:
 
     page_numbers = set()
     
-    for page in PDFPage.get_pages(pdf_infile, page_numbers, maxpages=0, password=password, caching=True, check_extractable=False):
-        interpreter.process_page(page)
-
+    if password is None:
+        for page in PDFPage.get_pages(pdf_infile, page_numbers, maxpages=0, caching=True, check_extractable=False):
+            interpreter.process_page(page)
+    else:
+        for page in PDFPage.get_pages(pdf_infile, page_numbers, maxpages=0, password=password, caching=True, check_extractable=False):
+            interpreter.process_page(page)
+    
     text = output.getvalue()
     pdf_infile.close()
     device.close()
@@ -59,12 +56,12 @@ def extract_protected_pdf_text(path: str, password: str) -> str:
 """ Returns a filename based on extracted text from pdf file provided in text argument   
 
 """
-def create_file_name(text: str) -> str:
+def create_file_name(text_or_filename: str) -> str:
     
     index_string = 'Udskrevet '
     
-    if index_string in text:
-        pdf_text = text
+    if index_string in text_or_filename:
+        pdf_text = text_or_filename
 
         index_string_lenght = len(index_string)
         temp_date_string = pdf_text[pdf_text.index(index_string)+index_string_lenght:-1]
@@ -77,9 +74,14 @@ def create_file_name(text: str) -> str:
         # example: 2015-november-1.pdf
         file_name_string = '{0:%Y}-{0:%B}-{1}.pdf'.format(printed_date, 1)
     else:
-        file_name = text
-        printed_date = parse(file_name)
-        file_name_string = '{0:%Y}-{0:%B}-{1}.pdf'.format(printed_date, 1)
+        file_name = text_or_filename
+
+        temp_list = file_name.split('\\')
+        temp_string = temp_list[-1]
+        
+        temp_date = temp_string[0:temp_string.index('_', 0, -1)]
+                       
+        file_name_string = temp_date + '-1.pdf'
         
     return file_name_string
 
@@ -111,23 +113,24 @@ def main():
     for file in files:
         
         if 'Daniel_Jensen' in file:
-            file_name = create_file_name(file)
+            print ('Daniel_Jensen: ' + file)
+            file_name = create_file_name(text_or_filename = file)
+            output_file_name = 'pdfout\\' + file_name
+            print (output_file_name)
+        elif '22801 - Daniel - Lønspecifikation maj 2018.pdf' in file:
+            print('22801 - Daniel - Lønspecifikation maj 2018.pdf')
+            extracted_text = extract_protected_pdf_text(path=file, password=None)
+            file_name = create_file_name(text_or_filename = extracted_text)
+            output_file_name = 'pdfout\\' + file_name
+            print (output_file_name)
+        elif '22801' in file:
+            print('22801 baby: ' + file)
+            extracted_text = extract_protected_pdf_text(path=file, password='2505832461')
+            file_name = create_file_name(text_or_filename = extracted_text)
             output_file_name = 'pdfout\\' + file_name
             print (output_file_name)
         else:
-            print('Shouldnt see me: ' + file)
-        # elif 'DAJE' in file:
-        #     extracted_text = extract_protected_pdf_text(path=file, password=)
-        #     file_name = create_file_name(text = extracted_text)
-        #     output_file_name = 'pdfout\\' + file_name
-        #     print (output_file_name)
-        
-    
-    # extracted_text = extract_protected_pdf_text(path = 'pdfin\\15935_22801.pdf')
-    # file_name = create_file_name(text = extracted_text)
-    # output_file_name = 'pdfout\\' + file_name
-    
-    # remove_password_and_save(input_path = 'pdfin\\15935_22801.pdf', output_path = output_file_name)
+            print('File we do not handle: ' + file)
 
 if __name__ == '__main__':
     main()
